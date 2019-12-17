74870b307b6cc6efcbe217545b87d42f4165d0bc
Kyrokis <kyrokis@gmail.com>
2019-12-17 19:07:09 +0300

    Added support https

--- components/thread/Thread.php
+++ components/thread/Thread.php
@@ -62,15 +62,15 @@ Class Thread {
 				$url = \yii\helpers\Url::to([], true);
 				$ch = curl_init();
 				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($ch, CURLOPT_HEADER, 0);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_HEADER, false);
 				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 				curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-				curl_setopt($ch,CURLOPT_HTTPHEADER,
-					array('PHPThreads: true')
-				);
-				curl_setopt($ch, CURLOPT_POST, 1);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, ['PHPThreads: true']);
+				curl_setopt($ch, CURLOPT_POST, true);
 				curl_setopt($ch, CURLOPT_POSTFIELDS, 
 					'PHPThreads_Run='.urlencode(base64_encode($this->strcode($thread, $this->password))).'&PHPThreads_Session='.urlencode(base64_encode($this->strcode($session, $this->password)))
 				);
@@ -100,7 +100,7 @@ Class Thread {
 							$resp[$url] = $curl_result['return'];
 							
 							curl_multi_remove_handle($cmh, $ch);
-							curl_close($ch);							
+							curl_close($ch);
 						}
 					}
 					while ($mrc == CURLM_CALL_MULTI_PERFORM);
